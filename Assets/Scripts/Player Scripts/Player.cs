10d2dee7899420b4de66bef8037223ac4a01e06a
Nadia Addington <146094382+Cadecamia@users.noreply.github.com>
2025-03-04 16:53:59 -0500

    Player is now slower (Hopefully)

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -7,7 +7,7 @@ using UnityEngine.UIElements;
 public class Player : MonoBehaviour
 {
 
-    public float movementSpeed = 0.1f;
+    public float movementSpeed = 0.01f;
 
     // Start is called before the first frame update
     void Start()

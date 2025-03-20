70ee488d90449f1277129c0ea2334d2420b203be
Jamcat42 <jamcatjunk@outlook.com>
2025-03-20 16:44:33 -0400

    enemy sight basics

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -15,11 +15,18 @@ public class Enemy : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false; //May need to be changed later
         agent.updateUpAxis = false;
+        agent.speed = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(target.position);
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y), Vector2.left, 5.0f);
+
+        if (hit && hit.collider.tag == "Player") 
+        {
+            agent.speed = 5.0f;       
+        }
     }
 }

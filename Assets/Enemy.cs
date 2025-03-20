e2433f6d165c0847337e8a7e28e4b5165eefb4e3
Jamcat42 <jamcatjunk@outlook.com>
2025-03-20 16:47:52 -0400

    Update Enemy.cs

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -22,7 +22,7 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         agent.SetDestination(target.position);
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y), Vector2.left, 5.0f);
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y), Vector2.left, 5.0f); //Change the Vector2.left to whatever direction we want default forward to be.
 
         if (hit && hit.collider.tag == "Player") 
         {

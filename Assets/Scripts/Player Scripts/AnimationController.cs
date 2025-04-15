d1381e1278368f8ff5b163f6abd86601cff610ea
Alexys Lupu <158080941+LupuChanTM@users.noreply.github.com>
2025-04-15 19:21:25 -0400

    walk animations done

--- Assets/Scripts/Player Scripts/AnimationController.cs
+++ Assets/Scripts/Player Scripts/AnimationController.cs
@@ -4,21 +4,69 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
-    private Animation anim;
+    private Animator anim;
+    private int lastDirection = 0; //  0 ^, 1 \/, 2 <, 3 >
+
     void Start()
     {
-        anim = gameObject.GetComponent<Animation>();
+        anim = gameObject.GetComponent<Animator>();
     }
+    float horizontalInput;
+    float verticalInput;
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.U))
+        StartMoving();
+    }
+
+    void StartMoving()
+    {
+        if (verticalInput > 0)
         {
             anim.Play("BackwardsWalk");
+            lastDirection = 0;
         }
+        else if (verticalInput < 0)
+        {
+            anim.Play("ForwardsWalk");
+            lastDirection = 1;
+        }
+        else if (horizontalInput > 0)
+        {
+            anim.Play("RightWalk");
+            lastDirection = 2;
+        }
+        else if (horizontalInput < 0)
+        {
+            anim.Play("LeftWalk");
+            lastDirection = 3;
+        }
+        else if (horizontalInput == 0 && verticalInput == 0)
+        {
+            StopMoving();
+        }
+    }
 
+    void StopMoving()
+    {
+        if (lastDirection == 0)
+        {
+            anim.Play("IdleBackwards");
+        }
+        else if (lastDirection == 1)
+        {
+            anim.Play("IdleForwards");
+        }
+        else if (lastDirection == 2)
+        {
+            anim.Play("IdleRight");
+        }
+        else if (lastDirection == 3)
+        {
+            anim.Play("IdleLeft");
+        }
     }
 }
